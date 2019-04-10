b992bd6c371386e4a3f15cd9b625fe564a460106
Jan Stangenberg <jan.stangenberg@gmx.de>
2019-04-10 02:01:12 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+/**
+ * globals
+ */
+var app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    session = require("express-session")({
+        secret: "my-secret",
+        resave: true,
+        saveUninitialized: true
+    }),
+    sharedsession = require("express-socket.io-session");
+
+/**
+ * port call fallback
+ */
+app.get('/', function(req, res) {
+    res.send('<script>window.location = "http://localhost:3333/"</script>');
+});
+
+/**
+ * session provider
+ */
+app.use(session);
+io.use(sharedsession(session));
+
+/**
+ * connection based events
+ */
+io.on('connection', (socket) => {    
+    
+    /**
+     * debug
+     */
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    /**
+     * session handling
+     */
+    socket.on("login", function(userdata) {
+        socket.handshake.session.userdata = userdata;
+        socket.handshake.session.save();
+    });
+    socket.on("logout", function(userdata) {
+        if (socket.handshake.session.userdata) {
+            delete socket.handshake.session.userdata;
+            socket.handshake.session.save();
+        }
+    });
+
+    /**
+     * chat messaging
+     */
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('application:loaded', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });    
+});
+
+/**
+ * with arms wide open
+ */
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
