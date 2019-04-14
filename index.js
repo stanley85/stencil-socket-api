4a9e17ce1ea4f120ed5349cd38d0edbe5d10a5aa
Jan Stangenberg <jan.stangenberg@gmx.de>
2019-04-14 20:51:46 +0200

    mongo db connection established II

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("mydb");
-  dbo.collection("users").findOne({}, function(err, result) {
+  var dbo = db.db("ssb");
+  dbo.collection("user").findOne({}, function(err, result) {
     if (err) throw err;
-    console.log(result);
+    console.log(result.name);
     db.close();
   });
 });
